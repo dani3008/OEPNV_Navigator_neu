6285bc7ceaa5975afccc7e4691bbeddb2c43f7cd
wada1028 <wada1028@hs-karlsruhe.de>
2019-12-17 11:01:16 +0100

    Stopfinder with server request autocomplete

--- app/src/main/java/wada1028/info3/oepnv_navigator/ui/home/HomeFragment.java
+++ app/src/main/java/wada1028/info3/oepnv_navigator/ui/home/HomeFragment.java
@@ -1,6 +1,9 @@
 package wada1028.info3.oepnv_navigator.ui.home;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,56 +25,102 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 import wada1028.info3.oepnv_navigator.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    private List<String> halteList = new ArrayList<>();
     private RequestQueue queue;
-    private static final String[] STOPS = new String[]{"Europaplatz","Markplatz","Herrenstraße"};
+    private AutoCompleteTextView autoCompleteTextViewStart;
+    private AutoCompleteTextView autoCompleteTextViewZiel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-            ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+
+    //private static final String[] STOPS = new String[]{"Europaplatz","Markplatz","Herrenstraße"};
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
-        AutoCompleteTextView autoCompleteTextViewStart = root.findViewById(R.id.autoCompleteTextView_Starthaltestelle);
-        AutoCompleteTextView autoCompleteTextViewZiel = root.findViewById(R.id.autoCompleteTextView_zielhaltestelle);
+        autoCompleteTextViewStart = root.findViewById(R.id.autoCompleteTextView_Starthaltestelle);
+        autoCompleteTextViewZiel = root.findViewById(R.id.autoCompleteTextView_zielhaltestelle);
 
-        // Server request
-        queue = Volley.newRequestQueue(getContext());
-            jsonParse();
+        autoCompleteTextViewStart.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-        //Stopfinder Autocomplete
+            }
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(),android.R.layout.simple_list_item_1,STOPS);
-        autoCompleteTextViewStart.setAdapter(arrayAdapter);
-        autoCompleteTextViewZiel.setAdapter(arrayAdapter);
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (autoCompleteTextViewStart.getText().length() >= 3) {
+                    jsonParse(autoCompleteTextViewStart);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        autoCompleteTextViewZiel.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (autoCompleteTextViewZiel.getText().length() >= 3) {
+                    jsonParse(autoCompleteTextViewZiel);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
         return root;
     }
-    private void jsonParse(){
-        String url = "http://smartmmi.demo.mentz.net/smartmmi/XML_STOPFINDER_REQUEST?outputFormat=rapidJson&type_sf=any&name_sf=hbf";
-        JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+    private void jsonParse(AutoCompleteTextView aCTextView) {
+        Log.i("METHODE", "in jsonParse");
+        queue = Volley.newRequestQueue(getContext());
+        String url = "http://smartmmi.demo.mentz.net/smartmmi/XML_STOPFINDER_REQUEST?outputFormat=rapidJson&type_sf=any&name_sf=" + aCTextView.getText().toString();
+        final JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
+                Log.i("METHODE", "in OnResponse");
                 try {
                     JSONArray jsonArray = response.getJSONArray("locations");
-                    for (int i=0; i < jsonArray.length();i++){
+                    for (int i = 0; i < jsonArray.length(); i++) {
                         JSONObject location = jsonArray.getJSONObject(i);
-                        String id = location.getString("id");
+                        //String id = location.getString("id");
                         String name = location.getString("name");
-                        String type =location.getString("type");
+                        //String type =location.getString("type");
+                        halteList.add(name);
+                        Log.i("METHODE", "" + name);
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
+                    Log.i("METHODE", "in catch");
                 }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 error.printStackTrace();
+                Log.i("METHODE", "Error: no connection");
             }
         });
+        queue.add(objectRequest);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_list_item_1, halteList);
+        aCTextView.setAdapter(arrayAdapter);
     }
 }
