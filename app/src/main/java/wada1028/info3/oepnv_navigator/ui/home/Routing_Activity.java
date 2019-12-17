51834d4203a0c13a69a925b79e53720a39951169
wada1028 <wada1028@hs-karlsruhe.de>
2019-12-17 12:24:42 +0100

    Map Layout

--- app/src/main/java/wada1028/info3/oepnv_navigator/ui/home/Routing_Activity.java
+++ app/src/main/java/wada1028/info3/oepnv_navigator/ui/home/Routing_Activity.java
@@ -4,13 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.MapboxAccountManager;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
 import wada1028.info3.oepnv_navigator.R;
 
 public class Routing_Activity extends AppCompatActivity {
+    private MapView mapview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MapboxAccountManager.start(this, getString(R.string.access_token));
         setContentView(R.layout.activity_routing_);
+        mapview = (MapView) findViewById(R.id.mapView);
+
+        mapview.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+
+            }
+        });
+        mapview.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapview.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapview.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapview.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapview.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapview.onLowMemory();
     }
 }
